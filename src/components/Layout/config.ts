406a13fea3a5ab37096e705269833bd1f3c01a9a
Johan Soulet <johan@troopers.email>
2020-12-02 08:30:38 +0100

    :lipstick: review SEO description

--- src/components/Layout/config.ts
+++ src/components/Layout/config.ts
@@ -2,7 +2,7 @@ export default {
   url: "https://christmas.johansoulet.fr/",
   title: "Calendrier de l'Avent 2020",
   description:
-    "Chaque jour avant Noël, un nouveau contenu tech, insirant, culturel, fun... à découvrir",
+    "Chaque jour, je vous propose un contenu tech, inspirant, culturel, fun...",
   author: "Johan Soulet",
   twitter: '@johansoulet',
   previewImage: "https://images.unsplash.com/photo-1543772857-a9d7ebc0cf63"

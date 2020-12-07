44c333b5d5063c0c6084a5ffc4a2fa6d9eb24378
Johan Soulet <johan@troopers.email>
2020-12-07 08:52:30 +0100

    :construction_worker: fix broken build

--- src/components/Post/CodeBlock/index.tsx
+++ src/components/Post/CodeBlock/index.tsx
@@ -1,7 +1,7 @@
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { tomorrow } from "react-syntax-highlighter/dist/cjs/styles/prism"
 
-const CodeBlock = ({ children, className, ...props }) => {
+const CodeBlock = ({ children, className = "", ...props }) => {
   const language = className.replace(/language-/, "")
   return (
     <SyntaxHighlighter

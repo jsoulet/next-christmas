8b581b51741e97001365f51fa0318c88cd711870
Johan Soulet <johan@troopers.email>
2020-12-07 01:19:23 +0100

    :pencil: day 8

--- src/components/Post/CodeBlock/index.tsx
+++ src/components/Post/CodeBlock/index.tsx
@@ -1,9 +1,14 @@
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { tomorrow } from "react-syntax-highlighter/dist/cjs/styles/prism"
 
-const CodeBlock = ({ children }) => {
+const CodeBlock = ({ children, className, ...props }) => {
+  const language = className.replace(/language-/, "")
   return (
-    <SyntaxHighlighter language="javascript" style={tomorrow}>
+    <SyntaxHighlighter
+      language={language || "javascript"}
+      style={tomorrow}
+      {...props}
+    >
       {children}
     </SyntaxHighlighter>
   )
